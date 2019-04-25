ab19aefc13f30c01cd4d6b1c8da39f7ede6d541d
rsullivan2704 <rsullivan2704@gmail.com>
2019-04-25 09:57:06 -0400

    Altered code to reflect icon image type more accurately. Previous implementation was more like generic image.

--- src/wpf/toga_wpf/icons.py
+++ src/wpf/toga_wpf/icons.py
@@ -1,19 +1,15 @@
 import os
 import toga
-from toga_wpf.libs import System, WPF
-from toga_wpf.libs.Media import Imaging
+from toga_wpf.libs import System
+from toga_wpf.libs import Imaging
 
 
 class Icon:
+
+    EXTENSIONS = ('.bmp', '.ico', 'png')
+
     def __init__(self, interface: toga.Icon) -> None:
         self.interface = interface
-        self.interface._impl = self
-        valid_extensions = ('.bmp', '.gif', '.ico', '.jpg', 'png', '.wdp', '.tiff')  # noqa: E501
-        file_path, file_extension = os.path.splitext(self.interface.filename)
-        bi = Imaging.BitmapImage()
-        if file_extension in valid_extensions:
-            bi.BeginInit()
-            bi.UriSource = System.Uri(self.interface.filename, System.UriKind.RelativeOrAbsolute)  # noqa: E501
-            bi.EndInit()
-        self.native = WPF.Controls.Image()
-        self.native.Source = bi
+        file_path, file_extension = (os.path.splitext(self.interface.filename))
+        if file_extension in self.EXTENSIONS:
+            self.native = Imaging.BitmapFrame().Create(System.Uri(self.interface.filename, System.UriKind.RelativeOrAbsolute))  # noqa: E501
