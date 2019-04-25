39467eb70a15b9dc58042bf86f8fc03db860a425
rsullivan2704 <rsullivan2704@gmail.com>
2019-04-25 09:59:12 -0400

    implemented add_child method

--- src/wpf/toga_wpf/widgets/box.py
+++ src/wpf/toga_wpf/widgets/box.py
@@ -4,6 +4,9 @@ from .base import Widget
 
 
 class Box(Widget):
+
     def create(self) -> None:
         self.native = WPF.Controls.StackPanel()
-        self.native.interface = self.interface
+
+    def add_child(self, child: Widget) -> None:
+        self.native.Children.Add(child.native)
