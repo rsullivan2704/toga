2e298edf90ca39e5000e0a25a3a396edfcf1a1c0
rsullivan2704 <rsullivan2704@gmail.com>
2019-04-25 10:00:24 -0400

    Continuing implementation of Command

--- src/wpf/toga_wpf/command.py
+++ src/wpf/toga_wpf/command.py
@@ -4,19 +4,40 @@ from .libs import System, Input
 
 
 class DelegateCommand(Input.ICommand):
-    def __init__(self, execute: Input.Action[object], can_execute: System.Predicate[object] = None) -> None:  # noqa: E501
-        self._execute = execute
-        self._can_execute = can_execute
+    # TODO: Have to figure this out
+    __namespace__ = 'toga_wpf'
 
-    def can_execute(self, parameter: object) -> bool:
-        if self._can_execute is None:
-            return True
-        return self._can_execute(parameter)
+    def __init__(self) -> None:
+        self.CanExecuteChanged += self._can_execute_changed
 
-    def execute(self, parameter: object) -> None:
-        self._execute(parameter)
+    def _can_execute_changed(sender, args) -> None:
+        pass
+
+    def CanExecute(self, parameter: System.Object) -> bool:
+        return True if self._can_execute is None else self._can_execute(parameter)  # noqa: E501
+
+    def set_canexecute(self, predicate: System.Predicate[System.Object]) -> None:  # noqa: E501
+        self._can_execute = predicate
+
+    def Execute(self, parameter: System.Object) -> None:
+        try:
+            self._execute(parameter)
+        except AttributeError:
+            pass
+
+    def set_execute(self, action: System.Action[System.Object]) -> None:
+        self._execute = action
 
 
 class Command:
     def __init__(self, interface: toga.Command) -> None:
         self.interface = interface
+        self.native = None
+        if self.interface.action is not None:
+            def can_execute(interface: toga.Command) -> bool:
+                return interface.enabled
+            action = System.Action(self.interface.action)
+            predicate = System.Predicate[System.Object](can_execute)
+            self.native = DelegateCommand()
+            self.native.set_canexecute(predicate)
+            self.native.set_execute(action)
