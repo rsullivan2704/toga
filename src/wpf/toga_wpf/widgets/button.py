e5a4166637cc3cd74d9a47258c7bdea4be1ddeba
rsullivan2704 <rsullivan2704@gmail.com>
2019-04-25 09:59:58 -0400

    extracted initialization into separate class (as in winforms impl.

--- src/wpf/toga_wpf/widgets/button.py
+++ src/wpf/toga_wpf/widgets/button.py
@@ -1,14 +1,33 @@
-import types
-from toga_wpf import WPF
+import toga
+
+import typing
+
+from toga.handlers import wrapped_handler
+
+from toga_wpf.libs import WPF
+
 from .base import Widget
+
+
+class WPFButton(WPF.Controls.Button):
+    def __init__(self, interface: toga.Button) -> None:
+        super().__init__()
+        self.interface = interface
+        self.Click += self.on_click
+
+    def on_click(self, sender: WPF.Controls.Button, eventargs: WPF.RoutedEventArgs) -> None:  # noqa: E501
+        try:
+            self.interface.on_press(self.interface)
+        except AttributeError:
+            pass
 
 
 class Button(Widget):
     def create(self) -> None:
-        self.native = WPF.Controls.Button()
+        self.native = WPFButton(self.interface)
 
     def set_label(self, label: object) -> None:
         self.native.Content = label
 
-    def set_on_press(self, handler: types.FunctionType) -> None:
-        self.native.Click += handler
+    def set_on_press(self, handler: type(wrapped_handler)) -> None:  # noqa: E501
+        pass
