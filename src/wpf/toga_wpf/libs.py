f6d8dc6c5321f851c56ce5e111adf43c8482dea3
rsullivan2704 <rsullivan2704@gmail.com>
2019-04-25 09:57:55 -0400

    refactored imports

--- src/wpf/toga_wpf/libs.py
+++ src/wpf/toga_wpf/libs.py
@@ -1,8 +1,13 @@
-import clr
+import os
 import sys
+
+import clr
+
 if sys.platform.lower() not in ['cli', 'win32']:
     print("WPF is only supported on Windows platform")
-clr.AddReference("C:\\Windows\\Microsoft.NET\\assembly\\GAC_64\\PresentationCore\\v4.0_4.0.0.0__31bf3856ad364e35\\PresentationCore") # noqa: E501
+windir = os.environ['WINDIR']
+# clr.AddReference('{dir}\\Microsoft.NET\\Framework64\\v4.0.30319\\WPF\\PresentationFramework.dll'.format(dir=windir)) # noqa: E501
+clr.AddReference(r"wpf\PresentationFramework") # noqa: E501
 
 import System  # noqa: W0611
 import System.Windows as WPF  # noqa: W0611
@@ -10,6 +15,14 @@ from System import ArgumentException  # noqa: W0611
 from System import Enum  # noqa: W0611
 from System import Threading  # noqa: W0611
 from System.Windows import Input  # noqa: W0611
-from System.Windows import Media  # noqa: W0611
+from System.Windows.Media import Imaging  # noqa: W0611
 from System.Windows.Media import FontFamily  # noqa: W0611
 from System.Windows.Media import SolidColorBrush  # noqa: W0611
+
+
+def add_handler(action):
+
+    def handler(sender, event):
+        return action(None)
+
+    return handler
