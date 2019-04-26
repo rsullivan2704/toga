26b351af33e20b1d9c21ac78d5435dde494cfadb
rsullivan2704 <rsullivan2704@gmail.com>
2019-04-26 01:39:56 -0400

    Added typehints to implementation

--- src/wpf/toga_wpf/command.py
+++ src/wpf/toga_wpf/command.py
@@ -1,4 +1,5 @@
 import toga
+import typing
 
 from .libs import System, Input
 
@@ -9,22 +10,22 @@ class DelegateCommand(Input.ICommand):
     def __init__(self) -> None:
         self.can_execute_changed_handlers = []
 
-    def add_CanExecuteChanged(self, handler):
+    def add_CanExecuteChanged(self, handler: typing.Callable) -> None:
         self.can_execute_changed_handlers.append(handler)
 
-    def remove_CanExecuteChanged(self, handler):
+    def remove_CanExecuteChanged(self, handler: typing.Callable) -> None:
         self.can_execute_changed_handlers.remove(handler)
 
-    def Execute(self, parameter) -> None:
+    def Execute(self, parameter: object) -> None:
         self._execute(parameter)
 
-    def set_Execute(self, action) -> None:
+    def set_Execute(self, action: typing.Callable) -> None:
         self._execute = action
 
-    def CanExecute(self, parameter) -> bool:
+    def CanExecute(self, parameter: object) -> bool:
         return True if self._can_execute is None else self._can_execute(parameter)  # noqa: E501
 
-    def set_CanExecute(self, predicate) -> None:
+    def set_CanExecute(self, predicate: typing.Callable) -> None:
         self._can_execute = predicate
 
     def OnCanExecuteChanged(self) -> None:
@@ -42,8 +43,8 @@ class Command:
             self.native.set_Execute(self.action)
             self.native.set_CanExecute(self.predicate)
 
-    def action(self, parameter) -> None:
+    def action(self, parameter: object) -> None:
         self.interface.action(parameter)
 
-    def predicate(self, parameter) -> bool:
+    def predicate(self, parameter: object) -> bool:
         return self.interface.enabled
