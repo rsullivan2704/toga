38a69bb7d02c87d054b518ba782cd071190957ab
Robert J Sullivan <robertjonsullivan@gmail.com>
2019-05-02 16:00:45 -0400

    Added __logger__ calls Changed set_bounds implementation to reflect the use of the Canvas control for root elements Still tinkering with the layout and rehint methods

--- src/wpf/toga_wpf/widgets/base.py
+++ src/wpf/toga_wpf/widgets/base.py
@@ -1,11 +1,11 @@
-import builtins
 from typing import Union
 
 from travertino.constants import BOTTOM, CENTER, JUSTIFY, LEFT, RIGHT, TOP
+from travertino.size import at_least
 
 import toga
 from toga_wpf import colors, factory
-from toga_wpf.libs import WPF, ArgumentException, Enum, FontFamily
+from toga_wpf.libs import WPF, Controls, ArgumentException, Enum, System, __logger__
 
 
 class Widget:
@@ -13,13 +13,22 @@ class Widget:
         self.interface = interface
         self.interface._impl = self
 
-        self._container = None
         self.native = None
         self.create()
-        self.interface.style.reapply()
+        self.native.LayoutUpdated += self._size_changed_handler
+        # self.native.SizeChanged += self._size_changed_handler
+        # self.native.Loaded += self._loaded_handler
+        # self.interface.style.reapply()
+
+    def _loaded_handler(self, sender: System.Object, args: WPF.RoutedEventArgs) -> None:
+        self.rehint()
+
+    def _size_changed_handler(self, sender: System.Object, args: WPF.RoutedEventArgs) -> None:
+        self.rehint()
 
     def rehint(self) -> None:
-        pass
+        self.interface.intrinsic.width = at_least(self.native.DesiredSize.Width)
+        self.interface.intrinsic.height = at_least(self.native.DesiredSize.Height)
 
     def add_child(self, child: 'Widget') -> None:
         pass
@@ -33,7 +42,8 @@ class Widget:
     def set_enabled(self, value: bool) -> None:
         try:
             self.native.IsEnabled = value
-        except AttributeError:
+        except AttributeError as ex:
+            __logger__.info('AttributeError in set_enabled method call\n{exception}'.format(exception=str(ex)))
             pass
 
 # region Applicator Methods
@@ -42,21 +52,25 @@ class Widget:
         return 0
 
     def set_bounds(self, x: int, y: int, width: int, height: int) -> None:
-        if self.native:
+        try:
             # Root level widgets may require vertical adjustment to
             # account for toolbars, etc
-            if self.interface.parent is None:
-                vertical_shift = self.frame.vertical_shift
-            else:
-                vertical_shift = 0
+            # if self.interface.parent is not None:
+            #     vertical_shift = self.frame.vertical_shift
+            # else:
+            #     vertical_shift = 0
+
+            # only set the top and left positions if
+            # this is not a root element.
+            if self.interface.parent is not None:
+                Controls.Canvas.SetTop(self.native, x)
+                Controls.Canvas.SetLeft(self.native, y)
 
             self.native.Width = width
             self.native.Height = height
-            try:
-                self.native.Left = x
-                self.native.Top = y + vertical_shift
-            except AttributeError:
-                pass
+        except AttributeError as ae:
+            __logger__.info('AttributeError in set_bounds method call:\n{message}'.format(message=str(ae)))  # noqa: E501
+            pass
 
     def set_alignment(self, alignment: Union[LEFT, RIGHT, TOP, BOTTOM, CENTER, JUSTIFY]) -> None:  # noqa: E501
         if alignment == JUSTIFY:
@@ -73,12 +87,8 @@ class Widget:
             try:
                 align = Enum.Parse(WPF.VerticalAlignment, alignment, True)
                 self.native.VerticalAlignment = align
-            except (AttributeError, ArgumentException):
-                # The above calls will always fail since we cannot get the
-                # actual enum Type from pythonnet.
-                # TODO: Have to figure out how to parse a .net Enum from
-                # a text value, ignoring case
-                factory.not_implemented('Applicator.text_alignment')
+            except (AttributeError, ArgumentException) as ex:
+                __logger__.info('Error in set_alignment method call:\n{message}'.format(message=str(ex)))
 
     def set_hidden(self, hidden: bool) -> None:
         try:
@@ -87,7 +97,8 @@ class Widget:
             else:
                 visibility = WPF.Visibility.Visible
             self.native.Visibility = visibility
-        except AttributeError:
+        except AttributeError as ex:
+            __logger__.info('Error in set_alignment method call:\n{message}'.format(message=str(ex)))
             pass
 
     def set_font(self, font: toga.Font) -> None:
