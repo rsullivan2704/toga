14e69120c893eefc716feec0cdb17f13c2c5c689
Robert J Sullivan <robertjonsullivan@gmail.com>
2019-05-02 15:54:29 -0400

    Added __logger__ for debugging Added additional imports

--- src/wpf/toga_wpf/libs.py
+++ src/wpf/toga_wpf/libs.py
@@ -1,18 +1,27 @@
 import sys
+import logging
 
-import clr
+__logger__ = logging.getLogger('toga_wpf_logger')
+__logger__.setLevel(logging.DEBUG)
+__ch__ = logging.StreamHandler()
+__ch__.setLevel(logging.DEBUG)
+__formatter__ = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+__ch__.setFormatter(__formatter__)
+__logger__.addHandler(__ch__)
+
+import clr  # noqa: E402
 
 if sys.platform.lower() not in ['cli', 'win32']:
     print("WPF is only supported on Windows platform")
 clr.AddReference(r"wpf\PresentationFramework")
 
-import System  # noqa: W0611
-import System.Windows as WPF  # noqa: W0611
+import System  # noqa: E402
+import System.Windows as WPF    # noqa: E402, W0611
 from System import ArgumentException, Enum, Threading  # noqa: W0611
 from System.Drawing import Brushes  # noqa: W0611
 from System.Globalization import CultureInfo  # noqa: W0611
 from System.Windows import Controls, FontStretches, Input  # noqa: W0611
-from System.Windows.Media import Imaging, FontFamilyConverter, FontFamily, FormattedText, NumberSubstitution, Typeface, Color, SolidColorBrush  # noqa: W0611, E501
+from System.Windows.Media import Imaging, FontFamilyConverter, FontFamily, FormattedText, NumberSubstitution, Typeface, Color, SolidColorBrush, VisualTreeHelper  # noqa: W0611, E501
 
 
 def add_handler(action):
@@ -21,12 +30,3 @@ def add_handler(action):
         return action(None)
 
     return handler
-
-
-def get_clr_type(typ):
-    """ simulate typeof(T) or GetClrType from IronPython.
-
-    Horrible hack, redo
-    """
-    name = typ.__module__ + "." + typ.__name__
-    return System.Type.GetType(name)
